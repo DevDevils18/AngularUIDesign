1600b3afd4685bbd5880622e9774d0a3a29381a7
robot-88 <aishwarya.rao189@gmail.com>
2025-05-11 02:21:52 +0100

    CSS changes

--- SinglePageLayout/src/ProductHeader/product-header/product-header.component.ts
+++ SinglePageLayout/src/ProductHeader/product-header/product-header.component.ts
@@ -15,7 +15,7 @@ export class ProductHeaderComponent {
     storageOptions: ['1 TB', '256GB', '512GB'],
     colorOptions: ['All', 'Black', 'White'],
     priceRange: { min: 17190, max: 22038 },
-    description: 'Experience the next generation of mobile technology with the iPhone 16 Pro Max...',
+    description: 'Experience the next generation of mobile technology with the iPhone 16 Pro Max 256GB, equipped with the Apple A18 pro chip and fast 5G connection.',
     imageUrl: 'https://www.journaldugeek.com/app/uploads/2024/09/iphone-16-pro.jpg',
   };
 }
